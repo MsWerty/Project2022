68b99c85c9b55633989e0bf5beceff99a2de6f9e
Elena <lenochek7777777@gmail.com>
2022-10-25 13:58:57 +0300

    edit

--- jQuery/Index.js
+++ jQuery/Index.js
@@ -1,7 +1,15 @@
-$('#knopka').on ('click', (event) => {
+//с кнопкой
+
+/*$('#okno').on ('click', (event) => {
     $('#okno').fadeOut(3000)
 })
 
 $('#knopka').on('click', (event) => {
-    $('#okno').fadeIn(3000)
-})
+    $('#okno').fadeIn(3000)     
+})*/
+
+//без кнопки
+$('#okno').slideUp(3000, function (){
+      $('#okno').slideDown(3000)
+      
+});
